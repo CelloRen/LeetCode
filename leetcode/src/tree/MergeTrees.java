856a700aee1c4e4207f41f5c3f9989f728067d94
wenbo <939549708@qq.com>
2021-07-25 23:20:16 +0800

    617. 合并二叉树

---
+++ leetcode/src/tree/MergeTrees.java
@@ -0,0 +1,20 @@
+package tree;
+
+// 617. 合并二叉树
+// https://leetcode-cn.com/problems/merge-two-binary-trees/
+public class MergeTrees {
+
+    // 递归地合并即可
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+        TreeNode rootNode = new TreeNode(root1.val + root2.val);
+        rootNode.left = mergeTrees(root1.left, root2.left);
+        rootNode.right = mergeTrees(root1.right, root2.right);
+        return rootNode;
+    }
+}
