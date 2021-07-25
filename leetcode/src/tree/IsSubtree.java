2f5b1c7e38e41192d20f7541fb397da56ec224e9
wenbo <939549708@qq.com>
2021-07-25 22:12:37 +0800

    isSubTree, 暴力递归

---
+++ leetcode/src/tree/IsSubtree.java
@@ -0,0 +1,27 @@
+
+// 572. 另一棵树的子树
+// url: https://leetcode-cn.com/problems/subtree-of-another-tree/
+public class IsSubtree {
+    // 这里直接暴力递归，不是最优解
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        return dfs(root, subRoot);
+    }
+
+    private boolean dfs(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return false;
+        }
+        return isSameTree(root, subRoot) || dfs(root.left, subRoot) || dfs(root.right, subRoot);
+    }
+
+    private boolean isSameTree(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null) {
+            return true;
+        }
+        if (root == null || subRoot == null || root.val != subRoot.val) {
+            return false;
+        }
+        return isSameTree(root.left, subRoot.left) && isSameTree(root.right, subRoot.right);
+    }
+
+}
